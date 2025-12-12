370681ccd45554383aacf9aa90a807e59ee18bbb
HapticLabMember <149564589+HapticLabMember@users.noreply.github.com>
2025-12-12 14:54:11 +0900

    Fixed up InventoryManager.hpp

--- Onslaught!/InventoryManager.hpp
+++ Onslaught!/InventoryManager.hpp
@@ -1,35 +1,3 @@
-//// InventoryManager.hpp
-//#pragma once
-//#include <vector>
-//#include "Item.hpp"
-//
-//class InventoryManager {
-//public:
-//    static InventoryManager& GetInstance() {
-//        static InventoryManager instance;
-//        return instance;
-//    }
-//
-//    const std::vector<Item>& GetItems() const { return items; }
-//
-//    void AddItem(const Item& item) {
-//        // merge stackable items
-//        if (item.stackable) {
-//            for (auto& i : items) {
-//                if (i.id == item.id) {
-//                    i.quantity += item.quantity;
-//                    return;
-//                }
-//            }
-//        }
-//        items.push_back(item);
-//    }
-//
-//private:
-//    InventoryManager() {}
-//    std::vector<Item> items;
-//};
-
 #pragma once
 #include "Item.hpp"
 #include "ItemData.hpp"
@@ -37,18 +5,67 @@
 #include <iostream>
 
 struct InventorySlot {
-
+    ItemID id = ItemID:None;
+    int quantity = 0;
 };
 
 class InventoryManager {
 public:
-    void update(float dt, sf::Vector2f playerPos) {
+    InventoryManager(int maxSlots = 20)
+    : maxInventorySlots(maxSlots), inventory(maxSlots), isInventoryFull(false) {}
+
+    int addItem(ItemID id, int amount) {
+        if (id == ItemID::None || amound <= 0) return 0;
+        const bool stackable = ItemDatabase::get(id).stackable;
+        const int maxStack = ItemDatabase::get(id).maxStack;
+
+        int remaining = amount;
+
+        if(stackable) {
+            for(auto& slot : slots) {
+                // if item is the and less than maxStack
+                if(slot.id == id && slot.quantity < maxStack) {
+                    const int canAdd = std::min(remaining, (maxStack - slot.quantity) );
+                    slot.quantity += canAdd;
+                    remaining -= canAdd;
+                    // if all the items are added, return
+                    if (remaining == 0) return amount;
+                }
+            }
+        }
+        // if items are still remaining, add them to new stack
+        for(auto& slot : slots) {
+            if(remaining == 0) break;
+            if (slot.quantity == 0) {
+                slot.id = id;
+                const int toPlace = stackable ? std::min(remaining, maxStack) : 1;
+                slot.quantity = toPlace;
+                remaining -= toPlace;
+            }
+        }
+
+        isInventoryFull = !hasEmptySlot();
+        return amount - remaining;
+
     }
 
     void reset() {
-        items.clear();
+        for (auto& slot : slots) {
+            slot.id = ItemID::None;
+            slot.quantity = 0;
+        }
+        isInventoryFull = false;
     }
 
 private:
-    std::unordered_map<int, InventorySlot> inventory;
+    bool hasEmptySlot() const {
+        // returns true if there are any slots with nothing inside
+        for (const auto& slot : slots) {
+            if (slot.quantity == 0) return true;
+        }
+        return false;
+    }
+    int maxInventorySlots;
+    bool isInventoryFull;
+    std::vector<InventorySlot> inventory;
 };
